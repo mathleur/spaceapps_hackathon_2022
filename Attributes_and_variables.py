6d2b5077a53b77976a112415136e5608decd41e7
edsml-kl121 <yugiohtra@gmail.com>
2022-10-01 16:57:26 +0700

    information about data

--- Attributes_and_variables.py
+++ Attributes_and_variables.py
@@ -5,18 +5,35 @@ from spacepy import pycdf
 
 # Wind variables
 cdf_pycdf_wind = pycdf.CDF("wi_h2_mfi_20220101_v04.cdf")
-cdf_pycdf_mag = pycdf.CDF("wi_h2_mfi_20220101_v04.cdf")
 wind_features = []
-mag_features = []
+print("wind Features: \n")
 for feature in cdf_pycdf_wind:
-  print(feature, cdf_pycdf_wind[feature].attrs)
+  print("\n", feature, "\n")
+  print(cdf_pycdf_wind[feature].attrs)
+  # f.write(cdf_pycdf_wind[feature].attrs)
   wind_features.append(feature)
 
+
+print(f'There are {len(wind_features)} features including: ')
+print(cdf_pycdf_wind)
+
+# Magnetic variables
+cdf_pycdf_mag = pycdf.CDF("dscovr_h0_mag_20220101_v01.cdf")
+mag_features = []
+print("Magnetic Features: \n")
 for feature in cdf_pycdf_mag:
+  print("\n", feature, "\n")
+  print(cdf_pycdf_mag[feature].attrs)
+  # f.write(cdf_pycdf_wind[feature].attrs)
   mag_features.append(feature)
 
-print(wind_features)
-print(mag_features)
+
+print(f'There are {len(mag_features)} features including: ')
+# for feature in mag_features:
+#   print(feature)
+print(cdf_pycdf_mag)
+
+# print(mag_features)
 
 # cdf_pycdf2 = pycdf.CDF("dscovr_h0_mag_20220101_v01.cdf")
 # print(cdf_pycdf["BF1"].attrs)
