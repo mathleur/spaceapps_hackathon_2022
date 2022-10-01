34b942391be2302ae49182c9d63f94ae8c9029e2
mathleur <90444327+mathleur@users.noreply.github.com>
2022-10-01 10:01:53 +0100

    Update test_tensorflow_hackathon.py
    Downloads data as numpy array

--- test_tensorflow_hackathon.py
+++ test_tensorflow_hackathon.py
@@ -5,10 +5,12 @@ os.environ["CDF_LIB"] = "C:\\CDF\\lib"
 from spacepy import pycdf
 import cdflib
 
-cdf_pycdf1=cdflib.CDF("2022.cdf")[0]
 cdf_pycdf = pycdf.CDF("2022.cdf")
 print(cdf_pycdf)
 print(cdf_pycdf["B1F1"][1])
 tseries=cdf_pycdf["B1F1"]
 print(cdf_pycdf1[1])
+cdf_pycdf1=cdflib.CDF("2022.cdf")
+x = cdf_pycdf1.varget("B1F1", startrec = 0, endrec = 84000)
+print(type(x))
 #print(tseries.dtype)
